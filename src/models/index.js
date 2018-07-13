ed76fe0e38c97d293bb05efa28d25dc1869862d0
abedaliby <abedaliby@yahoo.com>
2018-07-13 23:40:31 +0200

    configured sequelize for pg

--- src/models/index.js
+++ src/models/index.js
@@ -21,17 +21,19 @@ else {
             config.db.password,
             config.db.options
         )
-    }
 
+        
 fs
-    .readdirSync(__dirname)
-    .filter((file) =>
-        file !== 'index.js'
-    )
-    .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file))
-        db[model.name] = model
-    })
+.readdirSync(__dirname)
+.filter((file) =>
+    file !== 'index.js'
+)
+.forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
+})
+
+    }
 
 Object.keys(db).forEach(function (modelName) {
     if ('associate' in db[modelName]) {
