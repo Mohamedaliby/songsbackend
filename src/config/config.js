670cc602cd6101af0eab99b941aa79ca0daa9506
abedaliby <abedaliby@yahoo.com>
2018-07-13 21:49:28 +0200

    github deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
 const path = require('path')
+require('dotenv').config();
 
 const config = {
-    port: 3000,
+    port: process.env.DATABASE_URL || 3000,
     db: {
-        database: process.env.DATABASE_URL || 'tutorial',
-        user: process.env.DB_USER || 'tutorial',
-        password: process.env.DB_PASS || 'tutorial',
+        database: process.env.DATABASE_URL ,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         options: {
             dialect: process.env.DIALECT || 'sqlite',
             host: process.env.HOST || 'localhost',
@@ -13,7 +14,14 @@ const config = {
         }
     },
     authentication: {
-        jwtSecret: process.env.JWT_SECRET || 'secret'
+        jwtSecret: process.env.JWT_SECRET
+    },
+    heroku: {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true //false
     }
 }
 module.exports = config;
