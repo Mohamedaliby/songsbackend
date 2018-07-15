bf036c98d069153331ec9cf6c570725f68790698
abedaliby <abedaliby@yahoo.com>
2018-07-15 09:11:27 +0200

    sequelize improved to use heroku

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const config = require('../config/config')
 const db = {}
 
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+// if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
     // the application is executed on Heroku ... use the postgres database
     var sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
       dialect:  'postgres',
@@ -20,27 +20,27 @@ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
         Song: sequelize.import('./Song'),
         User: sequelize.import('./User'),
       };
-}
-else {
-        var sequelize = new Sequelize(
-            config.db.database,
-            config.db.user,
-            config.db.password,
-            config.db.options
-        )
+// }
+// else {
+//         var sequelize = new Sequelize(
+//             config.db.database,
+//             config.db.user,
+//             config.db.password,
+//             config.db.options
+//         )
 
         
-fs
-.readdirSync(__dirname)
-.filter((file) =>
-    file !== 'index.js'
-)
-.forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
-})
+// fs
+// .readdirSync(__dirname)
+// .filter((file) =>
+//     file !== 'index.js'
+// )
+// .forEach((file) => {
+//     const model = sequelize.import(path.join(__dirname, file))
+//     db[model.name] = model
+// })
 
-    }
+//     }
 
 Object.keys(db).forEach(function (modelName) {
     if ('associate' in db[modelName]) {
