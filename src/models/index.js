89735b2d6b6698debc9f251b60b9b76042f3dd6b
abedaliby <abedaliby@yahoo.com>
2018-07-15 10:16:19 +0200

    trying sqlite

--- src/models/index.js
+++ src/models/index.js
@@ -6,39 +6,40 @@ const db = {}
 
 // if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    var sequelize = new Sequelize(process.env.DATABASE_URL , {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      logging:  true //false
-    }) 
-    const models = {
-        Bookmark: sequelize.import('./Bookmark'),
-        History: sequelize.import('./History'),
-        message: sequelize.import('./message'),
-        Song: sequelize.import('./Song'),
-        User: sequelize.import('./User'),
-      };
+    // var sequelize = new Sequelize(config.db.database,
+    // {
+    //   dialect:  'postgres',
+    //   protocol: 'postgres',
+    //   logging:  true //false
+    // }) 
+    // const models = {
+    //     Bookmark: sequelize.import('./Bookmark'),
+    //     History: sequelize.import('./History'),
+    //     message: sequelize.import('./message'),
+    //     Song: sequelize.import('./Song'),
+    //     User: sequelize.import('./User'),
+    //   };
 // }
 // else {
-//         var sequelize = new Sequelize(
-//             config.db.database,
-//             config.db.user,
-//             config.db.password,
-//             config.db.options
-//         )
+        var sequelize = new Sequelize(
+            config.db.database,
+            config.db.user,
+            config.db.password,
+            config.db.options
+        )
 
         
-// fs
-// .readdirSync(__dirname)
-// .filter((file) =>
-//     file !== 'index.js'
-// )
-// .forEach((file) => {
-//     const model = sequelize.import(path.join(__dirname, file))
-//     db[model.name] = model
-// })
+fs
+.readdirSync(__dirname)
+.filter((file) =>
+    file !== 'index.js'
+)
+.forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
+})
 
-//     }
+    // }
 
 Object.keys(db).forEach(function (modelName) {
     if ('associate' in db[modelName]) {
