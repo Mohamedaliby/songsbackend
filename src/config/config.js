9b1b6dfb8da03b33fd0682ce43fa04818cf42b5e
abedaliby <abedaliby@yahoo.com>
2018-07-16 07:29:36 +0200

    added NODE_EN dev&prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,38 +1,26 @@
 const path = require('path')
-// const dotenv = require('dotenv')
+if (process.env.NODE_ENV  == 'dev') {
+    const dotenv = require('dotenv')
 
-// if (dotenv.config()) {
-// const result = dotenv.config()
+    if (dotenv.config()) {
+        const result = dotenv.config()
 
-// if (result.error) {
-//  throw result.error
-// }
-// }
+        if (result.error) {
+            throw result.error
+        }
+    }
+}
 
 const config = {
     port: 3000,
     db: {
-        database: process.env.DATABASE_URL || 'postgres://xnambhdaayebef:127b17f798408d13da8222133c77e5895e481d69c207f9cda14c930a4d83784b@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d3uoq91r3cvaq2' ,
-        user: process.env.DB_USER || 'xnambhdaayebef',
-        password: process.env.DB_PASS || '127b17f798408d13da8222133c77e5895e481d69c207f9cda14c930a4d83784b',
+        database: process.env.DATABASE_URL,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         options: {
-            // dialect: process.env.DIALECT || 'sqlite' || 'postgres',
-            dialect: 'postgres',
-            protocol: 'postgres',
-            host: process.env.HOST || 'localhost' || 'ec2-54-225-103-255.compute-1.amazonaws.com',
-            port :'5432',
+            dialect: process.env.DIALECT || 'sqlite' || 'postgres',
+            host: process.env.HOST || 'localhost',
             storage: './tutorial.sqlit',
-        }
-    },
-    heroku: {
-        database: 'postgres://xnambhdaayebef:127b17f798408d13da8222133c77e5895e481d69c207f9cda14c930a4d83784b@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d3uoq91r3cvaq2' ,
-        user:'xnambhdaayebef',
-        password: '127b17f798408d13da8222133c77e5895e481d69c207f9cda14c930a4d83784b',
-        options: {
-            dialect: 'postgres',
-            protocol: 'postgres',
-            host: 'ec2-54-225-103-255.compute-1.amazonaws.com',
-            port :'5432',
         }
     },
     authentication: {
--- src/models/index.js
+++ src/models/index.js
@@ -4,66 +4,23 @@ const Sequelize = require('sequelize')
 const config = require('../config/config')
 const db = {}
 const Op = Sequelize.Op
-// if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    // var sequelize = new Sequelize(config.db.database,
-    // {
-    //   dialect:  'postgres',
-    //   protocol: 'postgres',
-    //   logging:  true //false
-    // }) 
-    // const models = {
-    //     Bookmark: sequelize.import('./Bookmark'),
-    //     History: sequelize.import('./History'),
-    //     message: sequelize.import('./message'),
-    //     Song: sequelize.import('./Song'),
-    //     User: sequelize.import('./User'),
-    //   };
-// }
-// else {
-        // var sequelize = new Sequelize(
-        //     config.heroku.database,
-        //     config.heroku.user,
-        //     config.heroku.password,
-        //     config.heroku.options,
-        //     config.heroku.options.operatorsAliases = {
-        //         $and: Op.and,
-        //         $or: Op.or,
-        //         $eq: Op.eq,
-        //         $gt: Op.gt,
-        //         $lt: Op.lt,
-        //         $lte: Op.lte,
-        //         $like: Op.like
-        //       },
-        //         // {
-        //         //     dialect:  'postgres',
-        //         //     protocol: 'postgres',
-        //         //     operatorsAliases: {
-        //         //       $and: Op.and,
-        //         //       $or: Op.or,
-        //         //       $eq: Op.eq,
-        //         //       $gt: Op.gt,
-        //         //       $lt: Op.lt,
-        //         //       $lte: Op.lte,
-        //         //       $like: Op.like
-        //         //     }
-        //         // }
-        // )
 
-const sequelize = new Sequelize('postgres://xnambhdaayebef:127b17f798408d13da8222133c77e5895e481d69c207f9cda14c930a4d83784b@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d3uoq91r3cvaq2',
-               {
-                    dialect:  'postgres',
-                    protocol: 'postgres',
-                    operatorsAliases: {
-                      $and: Op.and,
-                      $or: Op.or,
-                      $eq: Op.eq,
-                      $gt: Op.gt,
-                      $lt: Op.lt,
-                      $lte: Op.lte,
-                      $like: Op.like
-                    }
-                })
+
+
+if (process.env.NODE_ENV == 'production') {
+    const sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        operatorsAliases: {
+            $and: Op.and,
+            $or: Op.or,
+            $eq: Op.eq,
+            $gt: Op.gt,
+            $lt: Op.lt,
+            $lte: Op.lte,
+            $like: Op.like
+        }
+    })
     //      const models = {
     //     Bookmark: sequelize.import('./Bookmark.js'),
     //     History: sequelize.import('./History.js'),
@@ -73,24 +30,56 @@ const sequelize = new Sequelize('postgres://xnambhdaayebef:127b17f798408d13da822
     //   };
 
     sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection to database has been established successfully.');
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    })
-fs
-.readdirSync(__dirname)
-.filter((file) =>
-    file !== 'index.js'
-)
-.forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
-})
+        .authenticate()
+        .then(() => {
+            console.log('Connection to database has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        })
 
-    // }
+}
+// else {
+// var sequelize = new Sequelize(
+//     config.heroku.database,
+//     config.heroku.user,
+//     config.heroku.password,
+//     config.heroku.options,
+//     config.heroku.options.operatorsAliases = {
+//         $and: Op.and,
+//         $or: Op.or,
+//         $eq: Op.eq,
+//         $gt: Op.gt,
+//         $lt: Op.lt,
+//         $lte: Op.lte,
+//         $like: Op.like
+//       },
+//         // {
+//         //     dialect:  'postgres',
+//         //     protocol: 'postgres',
+//         //     operatorsAliases: {
+//         //       $and: Op.and,
+//         //       $or: Op.or,
+//         //       $eq: Op.eq,
+//         //       $gt: Op.gt,
+//         //       $lt: Op.lt,
+//         //       $lte: Op.lte,
+//         //       $like: Op.like
+//         //     }
+//         // }
+// )
+
+fs
+    .readdirSync(__dirname)
+    .filter((file) =>
+        file !== 'index.js'
+    )
+    .forEach((file) => {
+        const model = sequelize.import(path.join(__dirname, file))
+        db[model.name] = model
+    })
+
+// }
 
 Object.keys(db).forEach(function (modelName) {
     if ('associate' in db[modelName]) {
